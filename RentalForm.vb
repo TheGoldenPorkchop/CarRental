5955a7557baa74ffa0b34a2e92578d8b517eae43
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-04-14 08:50:25 -0600

    Innit

--- RentalForm.vb
+++ RentalForm.vb
@@ -2,5 +2,7 @@
 Option Strict On
 Option Compare Binary
 Public Class RentalForm
-
+    Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
+        'THIS IS A NEW CHANGE I GUESS
+    End Sub
 End Class
